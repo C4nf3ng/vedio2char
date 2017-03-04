777d14b6e8481d3ce69eee4332a8ef9072b57b28
C4nf3ng <C4nf3ng@users.noreply.github.com>
2017-03-05 02:02:57 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 import cv2
 from PIL import Image
 
@@ -8,27 +9,26 @@ def video_to_frame(videofile):
     while (video_cap.isOpened()):
         print('frame %d' % frame_count)
         ret, frame = video_cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("frame%d.png" % frame_count, gray)
+        if ret is False:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+        cv2.imwrite("frame%d.png" % frame_count, frame)
         frame_count += 1
     return frame_count
 
 def resize_frame(img):
-    frame = Image.open(img, 'r').convert("L")
-    frame = frame.resize(size)
-    return frame
+    return Image.open(img, 'r').convert("L").resize(size)
 
 def frame_to_char(frame_num):
     gray_char=['@','#','$','%','&','?','*','o','/','{','[','(',
                '|','!','^','~','-','_',':',';',',','.','`',' ']
-    count=len(gray_char)
     text = open("text1.txt", "w")
     for i in range(frame_num):
         frame = resize_frame('frame%d.png' % i)
         for y in range(size[1]):
             for x in range(size[0]):
                 gray = frame.getpixel((x,y))
-                char = gray_char[int(gray/(255/(count-1)))]
+                char = gray_char[int(gray/(255/(len(gray_char)-1)))]
                 text.write(char)
             text.write("\n")
         print("writing frame %d"%i)
@@ -36,6 +36,6 @@ def frame_to_char(frame_num):
     print('Done!')
 
 if __name__ == '__main__':
-    size = (236, 61) #size of output frame
-    nframe = video_to_frame('maid.flv') #video to convert
+    size = (236, 61) #size of output frame (x,y)
+    nframe = video_to_frame(sys.argv[1]) #video to convert
     frame_to_char(nframe)
